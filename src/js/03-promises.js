0c9bdc57dbd0fe7faf9671658b41b80f2c38f61b
Natali258 <natalya.holyavko@gmail.com>
2023-12-09 00:15:18 +0200

    hw-09 update4

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,8 +3,8 @@ const stepForm = document.querySelector('[name="step"]');
 const amountForm = document.querySelector('[name="amount"]');
 const btn = document.querySelector('button');
 
-function activePromise(event) {
-  event.preventDefault();
+function activePromise(e) {
+  e.preventDefault();
   let valueDelay = Number(delayForm.value);
   let valueStep = Number(stepForm.value);
   let valueAmount = Number(amountForm.value);
