d373420c2e15d67211f286d8462560a7ddedd440
Natali258 <141056126+Natali258@users.noreply.github.com>
2023-12-08 22:05:00 +0000

    Deploying to gh-pages from @ Natali258/goit-js-hw-09@6a738ff2eabb798afcaae3f7c473f80b13d3c002 🚀

---
+++ 01-color-switcher.f0b39c12.js
@@ -0,0 +1,2 @@
+!function(){let t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body"),a=null;t.addEventListener("click",()=>{t.disabled=!0,e.disabled=!1,a=setInterval(()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`},1e3)}),e.addEventListener("click",()=>{t.disabled=!1,e.disabled=!0,clearInterval(a)})}();
+//# sourceMappingURL=01-color-switcher.f0b39c12.js.map
